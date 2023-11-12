81709945618601bf8a8a12e52d921b70725a8a79
FAITH <faithokereke26@gmail.com>
2023-11-12 13:52:49 +0100

    commit changes

--- src/pages/main/Upload.tsx
+++ src/pages/main/Upload.tsx
@@ -1,17 +1,13 @@
 import React, { useRef } from "react";
+import Button from "../contact/ui/Button";
+import Input from "../../components/ui/Input";
 const Upload = () => {
-  const ref = useRef<HTMLInputElement>(null);
-  const openFile = () => {
-    if (ref.current) {
-      ref.current.click();
-    }
-  };
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
   };
   return (
     <>
-    <div className="p-2">
+    <div className="p-2 text-sm ">
     <p>Upload all documents here needed for the Clearance</p>
       <p>
         Note: Make sure they are authentic and from the appropriate websites
@@ -21,100 +17,85 @@ const Upload = () => {
     
       <h2 className="p-2 text-center text-lg font-bold">Upload Documents Here </h2>
       <div className="flex flex-col gap-4 m-2 ">
-        <div className="Jamb-documents">
-        <button className="text-black rounded-md border-2 p-2 text-sm hover:opacity-75" onClick={openFile}>Jamb Admission Letter(pdf only)</button>
-        <input
-        //   className="hidden"
+        <div className="">
+          <label htmlFor="admission-letter">Jamb Admission Letter</label>
+        <Input
           type="file"
-          ref={ref}
-          onChange={handleFileChange}
           name="admission_letter"
           id="jamb_admission"
           accept=".pdf"
-        />
-        <button className="text-black rounded-md border-2 p-2 text-sm hover:opacity-75" onClick={openFile}>WAEC Certificate</button>
-        <input
-        //   className="hidden"
+        ></Input>
+        <label htmlFor="jambresult">Jamb Original Result</label>
+        <Input
           type="file"
-          ref={ref}
-          onChange={handleFileChange}
-          name="admission_letter"
-          id="jamb_admission"
+          name="Jamb-result"
+          id="Jamb-Fresult"
+          accept=".pdf"
+        ></Input>
+        <label htmlFor="Waec">WAEC Original Certificate</label>
+        <Input
+          type="file"
+          name="waec"
+          id="waec"
           accept=".pdf,.jpg,.jpeg,.png"
-        />
-        <button className="text-black rounded-md border-2 p-2 text-sm hover:opacity-75" onClick={openFile}>Jamb Original Result(pdf only)</button>
-        <input
-        //   className="hidden"
-          type="file"
-          ref={ref}
-          onChange={handleFileChange}
-          name="admission_letter"
-          id="jamb_admission"
-          accept=".pdf"
-        />
-        </div> <hr />
+        ></Input>
+        </div> 
+        <hr />
       <div className="School-documents">
-      <button className="text-black rounded-md border-2 p-2 text-sm hover:opacity-75" onClick={openFile}>School Admission Letter(pdf only)</button>
-        <input
-        //   className="hidden"
+    <label htmlFor="school-admission">FUTO Admission Letter</label>
+        <Input
           type="file"
-          ref={ref}
-          onChange={handleFileChange}
           name="admission_letter"
-          id="jamb_admission"
+          id="school-admission"
           accept=".pdf"
-        />
-        
+        ></Input>
+          <label htmlFor="school-admission">Acceptance Letter</label>
+        <Input
+          type="file"
+          name="acceptance"
+          id="acceptance"
+          accept=".pdf"
+        ></Input>
+
+          <label htmlFor="school-admission">Validity Form</label>
+        <Input
+          type="file"
+          name="validty"
+          id="validity"
+          accept=".pdf"
+        ></Input>
+          <label htmlFor="school-admission">Form 19</label>
+        <Input
+          type="file"
+          name="19"
+          id="form_19"
+          accept=".pdf"
+        ></Input>
+          <label htmlFor="school-admission">POST-UTME Results</label>
+        <Input
+          type="file"
+          name="post"
+          id="post"
+          accept=".pdf"
+        ></Input>
       </div><hr />
       <div className="personal-files">
-      <button className="text-black rounded-md border-2 p-2 text-sm hover:opacity-75" onClick={openFile}>Birth Certificate</button>
-        <input
-        //   className="hidden"
+        <label>Birth Certifiacte</label>
+      <Input
           type="file"
-          ref={ref}
-          onChange={handleFileChange}
-          name="admission_letter"
-          id="jamb_admission"
-          accept=".pdf"
-        />
+          name="birth"
+          id="birth"
+          accept=".pdf,.png,.jpeg.jpeg"
+        ></Input>
+            <label>L.G.A Identification</label>
+      <Input
+          type="file"
+          name="identification"
+          id="lga"
+          accept=".pdf,.jpg,.jpeg,.png"
+        ></Input>
       </div>
-        {/* < input className=" text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Jamb Admission Letter
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Futo Admission Letter
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Post-Utme Results
-        
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Acceptance Letter
-        
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Confirmation of Admission
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          WAEC Original result
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Birth Certificate
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Validty Form
-        
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          Form 18
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"/>
-          L.G.A Identification Slip
-         
-        < input className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75">
-          Original Jamb Result
-         
-        < input type="file" className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75">
-          Passport Photograph
        
-        < className="  text-black rounded-md border-2 p-2 text-sm hover:opacity-75"></ input> */}
       </div>
     </>
   );
